6efd3357acbfc100332ff1acf5a38aa0656e4494
angga-22 <anggasp.unib@gmail.com>
2022-12-24 15:13:27 +0700

    feat: remove boilerplate templates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>{{ data }}</h1>
   </main>
 </template>
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HomeView",
+  setup() {
+    const data = "home";
+    return { data };
+  },
+});
+</script>
